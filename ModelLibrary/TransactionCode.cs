d391696d24cd3768f2b83c6655bad1ead8eeca68
nbasakuragi <nbasakuragi@gmail.com>
2014-05-20 00:38:19 +0100

    First Commit

---
+++ ModelLibrary/TransactionCode.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModelLibrary
+{
+    public enum TransactionType
+    {
+        Absence,
+        PayProtection
+    }
+    public class TransactionCode
+    {
+        public string Code { get; set; }
+
+        public TransactionType TransactionType { get; set; }
+    }
+}
---
+++ MongoDbAppTest/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using ModelLibrary;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization;
+using MongoDB.Driver;
+
+namespace MongoDbAppTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+       
+            var server = new MongoServer(new MongoServerSettings { Server = new MongoServerAddress("localhost") });
+            server.Connect();
+            var database = server.GetDatabase("CrewPay");
+
+            var transactionCodes = database.GetCollection<TransactionCode>("TransactionCodes");
+            
+            transactionCodes.CreateIndex("Code");
+
+            transactionCodes.InsertBatch(new List<TransactionCode> 
+            {
+                new TransactionCode{Code = "ABC", TransactionType = TransactionType.Absence},
+                new TransactionCode{Code = "DEF", TransactionType = TransactionType.Absence},
+                new TransactionCode{Code = "GHI", TransactionType = TransactionType.PayProtection}
+            });
+        }
+    }
+}
